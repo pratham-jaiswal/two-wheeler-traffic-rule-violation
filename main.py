b1723a38f31dd5c8c9d3dfe549bce86ea0a296f0
pratham-jaiswal <prathamj0502@gmail.com>
2023-10-25 23:35:01 +0530

    Fix: violated license plate printing

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 import requests
 import json
 import re
+from dotenv import load_dotenv
+load_dotenv()
 
 def ocr_space_file(filename, overlay, api_key, language):
 
@@ -22,9 +24,11 @@ def ocr_space_file(filename, overlay, api_key, language):
     data = json.loads(r.content.decode())
 
     lines = data["ParsedResults"][0]["TextOverlay"]["Lines"]
+
     lpnum = "".join(line["LineText"] for line in lines)
     lpnum = re.sub(r'[^a-zA-Z0-9]', '', lpnum)
     
+    return lpnum
 
 
 path = './models/512_2_5m.pt'
@@ -39,45 +43,45 @@ image = cv2.imread(image_path)
 image = cv2.resize(image, (1020, 600))
 
 results = model(image)
-
 frame = np.squeeze(results.render()[0])
 
 detections = results.pred[0]
-
 twowheelers = detections[(detections[:, 5] == 2) & (detections[:, 4] > 0.75)]
 
 count = 0
+violated_lp = []
 
 for tw in twowheelers:
     count += 1
-    print(1)
     x1, y1, x2, y2, conf, class_id = tw.tolist()
 
     helmets = detections[(detections[:, 5] == 0) & (detections[:, 4] > 0.5)]
     helmet_detected = False
     for helmet in helmets:
-        print(2)
         hx1, hy1, hx2, hy2, hconf, hclass_id = helmet.tolist()
         if x1 < hx1 < x2 and y1 < hy1 < y2:
             helmet_detected = True
-            print(3)
             break
 
     if not helmet_detected:
-        print(4)
         license_plates = detections[detections[:, 5] == 1]
         for license_plate in license_plates:
-            print(5)
             lx1, ly1, lx2, ly2, lconf, lclass_id = license_plate.tolist()
             if x1 < lx1 < x2 and y1 < ly1 < y2:
                 if conf > 0.75:
-                    print(6)
                     tw_image = frame[int(y1):int(y2), int(x1):int(x2)]
-                    output_path = os.path.join(output_dir, f"{count}_two_wheeler.jpg")
-                    cv2.imwrite(output_path, tw_image)
+                    tw_path = os.path.join(output_dir, f"{count}_two_wheeler.jpg")
+                    cv2.imwrite(tw_path, tw_image)
+
                     lp_image = frame[int(ly1):int(ly2), int(lx1):int(lx2)]
-                    output_path = os.path.join(output_dir, f"{count}_license_plate.jpg")
-                    cv2.imwrite(output_path, lp_image)
+                    lp_path = os.path.join(output_dir, f"{count}_license_plate.jpg")
+                    cv2.imwrite(lp_path, lp_image)
+
+                    lpnum = ocr_space_file(filename=lp_path, overlay=False, api_key=os.environ.get("OCR_SPACE_API"), language='eng')
+                    if lpnum not in violated_lp:
+                        violated_lp.append(lpnum)
+                        print("Violated Licenseplate: {}".format(lpnum))
+
 
     frame = np.squeeze(results.render()[0])
     cv2.imshow("Image", frame)
@@ -85,10 +89,6 @@ for tw in twowheelers:
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
-
-
-cv2.imshow("Image", frame)
-cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 
